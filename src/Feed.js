0179d8ee5dd5a01d0a82bce6a3972dda8086a7ae
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-06-07 21:46:43 +0100

    Build side bar

---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Feed() {
+    return (
+        <div>
+            {/* Header */}
+            <h2> This is the home page</h2>
+            {/* Tweet Box */}
+            {/* Post */}
+        </div>
+    )
+}
+
+export default Feed
