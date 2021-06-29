b78a773a3a99d665cc564ad59657a1d707966fa9
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-06-29 15:22:49 +0100

    Change order by and final part done and project complete

--- src/Feed.js
+++ src/Feed.js
@@ -1,42 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import './Feed.css'
-import TweetTextBox from './TweetTextBox'
-import Post from './Post'
-import FlareIcon from '@material-ui/icons/Flare';
-import db from './firebase';
-import FlipMove from 'react-flip-move'
+import React, { useState, useEffect } from "react";
+import "./Feed.css";
+import TweetTextBox from "./TweetTextBox";
+import Post from "./Post";
+import FlareIcon from "@material-ui/icons/Flare";
+import db from "./firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
 
-    const[posts, setPosts] = useState([]); 
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("text", "asc")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, []);
 
-    useEffect(() => {
-        db.collection('posts').onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => doc.data()))
-        ))
-    },[])
-    
-    return (
-        <div className="feed">
-            <div className="feed__header">
-                <h2>Home</h2><FlareIcon className="flareIconStyle" />
-            </div>
-            <TweetTextBox />
-            <FlipMove>
-            {posts.map(post => (
-                <Post 
-                    key={post.text}
-                    displayName={post.displayName}
-                    username={post.username}
-                    verified={post.verified}
-                    avatar={post.avatar}
-                    text={post.text}
-                    image={post.image}
-                />
-            ))} 
-            </FlipMove>
-        </div>
-    )
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+        <FlareIcon className="flareIconStyle" />
+      </div>
+      <TweetTextBox />
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            avatar={post.avatar}
+            text={post.text}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
